12855e3689292877a4bb3cd6ff2a1e02061fe376
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-11-11 19:11:00 -0300

    Moved frontend to new repo for easier deployment

--- frontend/src/lib/cartLib.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { baseServerUrl } from "../config/paths";
-
-export const addProductToCart = async (id, quantity) => {
-  axios.defaults.withCredentials = true;
-  const cartData = { productId: id, quantity: quantity };
-  const response = await axios.post(baseServerUrl + "/cart", cartData);
-  return response.data;
-};
--- frontend/src/lib/orderLib.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { baseServerUrl } from "../config/paths";
-
-export const sendNewOrder = async () => {
-  axios.defaults.withCredentials = true;
-  const response = await axios.post(baseServerUrl + "/order");
-  return response.data;
-};
